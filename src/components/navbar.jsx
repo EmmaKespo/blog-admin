d1ca6945f5acd8d4ebe9dc78f6264b747b05accd
EmmaKespo <kespoemma@gmail.com>
2026-07-02 15:58:14 +0100

    added nav bar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+export default function navbar() {
+  return (
+    <nav className="bg-slate-900 text-white p-4 flex justify-between items-center">
+        <h1 className="text-lg font-bold">Emmakespo</h1>
+        <span className="text-sm">Admin Panel</span>
+        </nav>
+      )  }
